fcc78e426a48f9c9980c39632e3f5d22213077cb
Yoshan Mumberson <mryosh@outlook.com>
2025-02-13 23:49:40 +0000

    Created project component

---
+++ src/app/components/Project.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ProjectProps {
+    name: string;
+    description: string;
+    children?: React.ReactNode;
+}
+
+const Project: React.FC<ProjectProps> = ({ name, description, children }) => {
+    return (
+        <div className="p-6 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-light leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{name}</h1>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">{description}</p>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <div className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Project;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import Card from "../components/Card";
 
 export default function Projects() {
     return (
-      <div className="">
+      <div className="sm:px-16 xl:px-48">
         <h1 className="text-center mb-4 text-3xl font-extrabold tracking-light leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Projects</h1>
         <div className="flex flex-col gap-2 md:flex-row items-center">
             <Card title="Title" href="/projects" hrefText="Projects" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex pariatur, consequatur dignissimos autem praesentium sunt ipsa soluta nobis at voluptatibus et veritatis. Dolores veritatis mollitia voluptate necessitatibus? Assumenda, accusamus fuga?" />
