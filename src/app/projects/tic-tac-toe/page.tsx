ef95bab392a6a6943ee61cec6cdc1b2a78310d3d
Yoshan Mumberson <mryosh@outlook.com>
2025-02-14 00:44:35 +0000

    Added functionality for drawing Xs into the board.

--- src/app/projects/tic-tac-toe/page.tsx
+++ src/app/projects/tic-tac-toe/page.tsx
@@ -1,26 +1,46 @@
+'use client';  // This marks the component as a Client Component
+
 import Project from "@/app/components/Project";
 import React from "react";
+import { useState } from "react";
 
 const Square = () => {
+    const [value, setValue] = useState("");
+
+    function handleClick() {
+        setValue("X");
+    }
+
     return (
-        <button className="border aspect-square">X</button>
-    )
+        <button 
+            className="border aspect-square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
+}
+
+const Board = () => {
+    return (
+        <div className="grid grid-cols-3 max-w-xs">
+            <Square/>
+            <Square/>
+            <Square/>
+            <Square/>
+            <Square/>
+            <Square/>
+            <Square/>
+            <Square/>
+            <Square/>
+        </div>
+    );
 }
 
 export default function TicTacToe() {
     return (
         <Project name="Tic-Tac-Toe" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe">
-            <div className="grid grid-cols-3 max-w-xs">
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-                <Square />
-            </div>
+            <Board />
         </Project>
     );
 }
