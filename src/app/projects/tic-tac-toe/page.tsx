077956b78415d34df6c07f4fd2dfb0b5af5f8f72
Yoshan Mumberson <mryosh@outlook.com>
2025-02-14 00:31:54 +0000

    Created grid

--- src/app/projects/tic-tac-toe/page.tsx
+++ src/app/projects/tic-tac-toe/page.tsx
@@ -1,10 +1,26 @@
 import Project from "@/app/components/Project";
 import React from "react";
 
+const Square = () => {
+    return (
+        <button className="border aspect-square">X</button>
+    )
+}
+
 export default function TicTacToe() {
     return (
         <Project name="Tic-Tac-Toe" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe">
-
+            <div className="grid grid-cols-3 max-w-xs">
+                <Square />
+                <Square />
+                <Square />
+                <Square />
+                <Square />
+                <Square />
+                <Square />
+                <Square />
+                <Square />
+            </div>
         </Project>
     );
 }
