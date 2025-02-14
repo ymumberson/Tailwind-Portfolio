65f780f844bb28b22d6e0fed8e9ca3b3a98e9967
Yoshan Mumberson <mryosh@outlook.com>
2025-02-14 00:10:35 +0000

    Setup project page for Tic-Tac-Toe

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,9 +6,7 @@ export default function Projects() {
       <div className="sm:px-16 xl:px-48">
         <h1 className="text-center mb-4 text-3xl font-extrabold tracking-light leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Projects</h1>
         <div className="flex flex-col gap-2 md:flex-row items-center">
-            <Card title="Title" href="/projects" hrefText="Projects" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex pariatur, consequatur dignissimos autem praesentium sunt ipsa soluta nobis at voluptatibus et veritatis. Dolores veritatis mollitia voluptate necessitatibus? Assumenda, accusamus fuga?" />
-            <Card title="Title" href="/projects" hrefText="Projects" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex pariatur, consequatur dignissimos autem praesentium sunt ipsa soluta nobis at voluptatibus et veritatis. Dolores veritatis mollitia voluptate necessitatibus? Assumenda, accusamus fuga?" />
-            <Card title="Title" href="/projects" hrefText="Projects" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex pariatur, consequatur dignissimos autem praesentium sunt ipsa soluta nobis at voluptatibus et veritatis. Dolores veritatis mollitia voluptate necessitatibus? Assumenda, accusamus fuga?" />
+            <Card title="Tic-Tac-Toe" href="/projects/tic-tac-toe" hrefText="view projct" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe" />
         </div>
       </div>
     );
---
+++ src/app/projects/tic-tac-toe/page.tsx
@@ -0,0 +1,10 @@
+import Project from "@/app/components/Project";
+import React from "react";
+
+export default function TicTacToe() {
+    return (
+        <Project name="Tic-Tac-Toe" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe">
+
+        </Project>
+    );
+}
