230c84f599750b7dcccc851b1354dcf206a10bdb
Yoshan Mumberson <mryosh@outlook.com>
2025-02-15 01:23:24 +0000

    Updated styling of projects page.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
     return (
       <div className="sm:px-16 xl:px-48">
         <h1 className="text-center mb-4 text-3xl font-extrabold tracking-light leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Projects</h1>
-        <div className="flex flex-col gap-2 md:flex-row items-center">
+        <div className="flex flex-col items-center">
             <Card title="Tic-Tac-Toe" href="/projects/tic-tac-toe" hrefText="view projct" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe" />
         </div>
       </div>
