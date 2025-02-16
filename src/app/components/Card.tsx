06cbb83a2b90b986b38ff7e032911846fe35cea7
Yoshan Mumberson <mryosh@outlook.com>
2025-02-16 13:36:20 +0000

    Updated icon for opening projects as previous icon implied opening a new page.

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { IconArrowNarrowRight } from "@tabler/icons-react";
 
 interface CardProps {
     title: string;
@@ -16,9 +17,7 @@ const Card: React.FC<CardProps> = ({ title, description, href, hrefText }) => {
             <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{description}</p>
             <Link href={href} className="inline-flex font-medium items-center text-blue-600 hover:underline">
                 {hrefText}
-                <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                </svg>
+                <IconArrowNarrowRight size={20}/>
             </Link>
         </div>
     );
