688b2c3b820150b668fe0cb7a0c5a412e9b271f7
Yoshan Mumberson <mryosh@outlook.com>
2025-02-18 23:51:10 +0000

    Added clear and undo buttons

--- src/app/projects/digit-classification/page.tsx
+++ src/app/projects/digit-classification/page.tsx
@@ -1,7 +1,8 @@
 'use client';  // This marks the component as a Client Component
 
 import Project from "@/app/components/Project";
-import React, { useRef, useState } from "react";
+import { IconChevronLeft, IconChevronRight, IconRefresh } from "@tabler/icons-react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface Vec2 {
     x: number;
@@ -51,9 +52,25 @@ const DigitCanvas: React.FC<DigitCanvasProps> = ({ width }) => {
         let newLines = [...lines, line];
         setLines(newLines);
         setBegin(end);
-        
+    };
+
+    const onUndo = () => {
+        if (lines.length === 0)
+            return;
+
+        let newLines = lines.slice(0, lines.length-1);
+        setLines(newLines);
+    }
+
+    const onReset = () => {
+        setBegin({x: 0, y: 0});
+        setLines([]);
+    }
+
+    const Draw = () => {
         if (canvasRef != null) {
             const context = canvasRef!.current!.getContext('2d');
+            context?.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
             lines.forEach((line, index) => {
                 context?.beginPath();
                 context?.moveTo(line.start.x, line.start.y);
@@ -61,21 +78,37 @@ const DigitCanvas: React.FC<DigitCanvasProps> = ({ width }) => {
                 context?.stroke();
             })
         }
-    };
+    }
     
-    return (
-        <canvas
-            ref={canvasRef}
-            id="DigitCanvas" 
-            width={width} 
-            height={width} 
-            className="border"
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}
-        >
+    useEffect(() => {
+        Draw();
+    }, [lines]);
 
-        </canvas>
+    return (
+        <div>
+            <canvas
+                ref={canvasRef}
+                id="DigitCanvas" 
+                width={width} 
+                height={width} 
+                className="border"
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseMove={handleMouseMove}
+            >
+            </canvas>
+            <div className="flex flex-row mb-5 justify-center gap-2">
+                <button onClick={() => onUndo()} className="border-2 text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 focus-ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                    <IconChevronLeft size={30}/>
+                </button>
+                {/* <button onClick={() => {}} className="border-2 text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 focus-ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                    <IconChevronRight size={30}/>
+                </button> */}
+                <button onClick={() => onReset()} className="border-2 text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 focus-ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                    <IconRefresh size={30}/>
+                </button>
+            </div>
+        </div>
     );
 }
 const DigitClassification = () => {
