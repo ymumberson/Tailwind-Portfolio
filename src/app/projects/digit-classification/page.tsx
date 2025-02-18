a12dcc2d4196926074a67ae171cd717e56e13139
Yoshan Mumberson <mryosh@outlook.com>
2025-02-18 23:27:01 +0000

    Fixed drawing functionality. Working, but likely not very performant.

--- src/app/projects/digit-classification/page.tsx
+++ src/app/projects/digit-classification/page.tsx
@@ -3,38 +3,64 @@
 import Project from "@/app/components/Project";
 import React, { useRef, useState } from "react";
 
+interface Vec2 {
+    x: number;
+    y: number;
+}
+interface SetVec2 {
+    (x: number, y: number): void;
+}
+
+interface Line {
+    start: Vec2;
+    end: Vec2;
+}
+interface SetLine {
+    (start: Vec2, end: Vec2): void;
+}
+
 interface DigitCanvasProps {
     width: number;
 }
 
 const DigitCanvas: React.FC<DigitCanvasProps> = ({ width }) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
-    const [points, setPoints] = useState<number[][]>([]);
-    var begin = [0,0];
+    const [lines, setLines] = useState<Line[]>([]);
+    const [mouseDown, setMouseDown] = useState(false);
+    const [begin, setBegin] = useState<Vec2>({x: 0, y: 0});
 
     const handleMouseDown: React.MouseEventHandler<HTMLCanvasElement> = (e) => {
         console.log("Mouse down!");
-        begin = [e.nativeEvent.offsetX, e.nativeEvent.offsetY];
-        let newPoints = [...points, begin];
-        setPoints(newPoints);
+        setBegin({x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY});
+        setMouseDown(true);
     };
     
     const handleMouseUp: React.MouseEventHandler<HTMLCanvasElement> = (e) => {
         console.log("Mouse up!");
-
-        if (canvasRef != null) {
-            const context = canvasRef!.current!.getContext('2d');
-            points.forEach((point) => {
-                context?.beginPath();
-                context?.moveTo(point[0], point[1]);
-                context?.lineTo(0,0);
-                context?.stroke();
-            })
-        }
+        setMouseDown(false);
     };
     
     const handleMouseMove: React.MouseEventHandler<HTMLCanvasElement> = (e) => {
         console.log("Mouse move!");
+
+        if (!mouseDown)
+            return;
+
+        var end = {x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY};
+        var line = {start: begin, end: end};
+        let newLines = [...lines, line];
+        setLines(newLines);
+        setBegin(end);
+        
+        if (canvasRef != null) {
+            const context = canvasRef!.current!.getContext('2d');
+            lines.forEach((line, index) => {
+                context?.beginPath();
+                context?.moveTo(line.start.x, line.start.y);
+                context?.lineTo(line.end.x, line.end.y);
+                context?.stroke();
+            })
+        }
     };
     
     return (
