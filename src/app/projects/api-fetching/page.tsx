22c35872f21cc48c29758b22e9fff14cee016f2e
Yoshan Mumberson <mryosh@outlook.com>
2025-02-28 22:56:13 +0000

    Setup new page for project.

---
+++ src/app/projects/api-fetching/page.tsx
@@ -0,0 +1,12 @@
+import Project from "@/app/components/Project";
+import React from "react";
+
+const ApiFetching = () => {
+    return (
+        <Project name="API Fetching" description="Fetching various bits of data from different APIs.">
+            
+        </Project>
+    );
+}
+
+export default ApiFetching;
