2d1b7514d35ecaa83cec2a7bacf7c92f500a8688
Yoshan Mumberson <mryosh@outlook.com>
2025-03-01 10:33:48 +0000

    Added placeholder data fetching using swr.

--- src/app/projects/api-fetching/page.tsx
+++ src/app/projects/api-fetching/page.tsx
@@ -1,10 +1,25 @@
+'use client';  // This marks the component as a Client Component
+
 import Project from "@/app/components/Project";
 import React from "react";
+import useSWR from "swr";
+
+const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    if (!response.ok) {
+        throw new Error('Network response was not okay');
+    }
+    return response.json();
+}
 
 const ApiFetching = () => {
+    const {data,error} = useSWR('dataKey', fetchData);
+
     return (
         <Project name="API Fetching" description="Fetching various bits of data from different APIs.">
-            
+            {!data && <p>Loading!</p>}
+            {error && <p>{error.message}</p>}
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         </Project>
     );
 }
