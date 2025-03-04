cf6a028716a5cbce8ea4041a6e56088af011428a
Yoshan Mumberson <mryosh@outlook.com>
2025-03-04 21:10:19 +0000

    Updated to use the 3.0 API instead of the free weather API.

--- src/app/projects/weather/page.tsx
+++ src/app/projects/weather/page.tsx
@@ -8,7 +8,7 @@ import { IconSunrise, IconSunset, IconWind, IconCloud, IconDroplet, IconSnowflak
 const fetchData = async () => {
     const lat = "52.1951";
     const lon = "0.1313";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`);
     if (!response.ok) {
         throw new Error('Network response was not okay');
     }
@@ -57,19 +57,20 @@ const Weather = (obj: any) => {
         <div className="flex flex-col items-center">
             <img className="rounded-xl" src={`https://openweathermap.org/img/wn/${obj.data.weather[0].icon}@2x.png`}></img>
             <div className="">
-                <h1 className="mb-0 text-3xl font-bold text-gray-900 dark:text-white">{obj.data.name}</h1>
+                <h1 className="mb-0 text-3xl font-bold text-gray-900 dark:text-white">Cambridge</h1>
                 <p>{obj.data.weather[0].main}: {obj.data.weather[0].description}</p>
-                <p className="">{Math.floor(KelvinToCelsius(obj.data.main.temp))}&#8451; ({Math.floor(KelvinToCelsius(obj.data.main.feels_like))}&#8451;)</p>
+                <p className="">{Math.floor(KelvinToCelsius(obj.data.temp))}&#8451; ({Math.floor(KelvinToCelsius(obj.data.feels_like))}&#8451;)</p>
                 <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-4 dark:bg-gray-700"/>
                 <div className="grid grid-cols-2">
-                    <WeatherBadge weatherIcon={IconSunrise} text={new Date(obj.data.sys.sunrise * 1000).toLocaleTimeString()}/>
-                    <WeatherBadge weatherIcon={IconSunset} text={new Date(obj.data.sys.sunset * 1000).toLocaleTimeString()}/>
-                    <WeatherBadge weatherIcon={IconWind} text={obj.data.wind.speed + "m/s"}/>
-                    <WeatherBadge weatherIcon={IconCloud} text={obj.data.clouds.all}/>
+                    <WeatherBadge weatherIcon={IconSunrise} text={new Date(obj.data.sunrise * 1000).toLocaleTimeString()}/>
+                    <WeatherBadge weatherIcon={IconSunset} text={new Date(obj.data.sunset * 1000).toLocaleTimeString()}/>
+                    <WeatherBadge weatherIcon={IconWind} text={obj.data.wind_speed + "m/s"}/>
+                    <WeatherBadge weatherIcon={IconCloud} text={obj.data.clouds}/>
                     <WeatherBadge weatherIcon={IconDroplet} text={obj.data.rain ? obj.data.rain["1h"] : "N/A"}/>
                     <WeatherBadge weatherIcon={IconSnowflake} text={obj.data.snow ? obj.data.snow["1h"] : "N/A"}/>
                 </div>
             </div>
+            {/* <pre>{JSON.stringify(obj, null, 2)}</pre> */}
         </div>
     );
 }
@@ -81,7 +82,7 @@ const ApiFetching = () => {
         <Project name="Weather" description="Fetching current weather data from https://openweathermap.org/ and displaying it.">
             {error && <ErrorMsg error={error} />}
             {!error && !data && <Loading />}
-            {!error && data && <Weather data={data} />}
+            {!error && data && <Weather data={data.current} />}
         </Project>
     );
 }
