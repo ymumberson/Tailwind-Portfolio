8ffc419f2977627ee1a830aaf5baecea409b420b
Yoshan Mumberson <mryosh@outlook.com>
2025-03-15 17:28:20 +0000

    Fetching commits for each repo belonging to the specified user.

--- src/app/sections/CommitGraph.tsx
+++ src/app/sections/CommitGraph.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from 'axios';
 import { IconUser } from "@tabler/icons-react";
 
@@ -7,23 +7,55 @@ interface UserProfilePhotoProps {
     username: string;
 }
 
-const UserProfilePhoto: React.FC<UserProfilePhotoProps> = ({ username }) => {
-    const [githubData, setGithubData] = useState<{[key: string]: string}>({});
+interface Repository {
+    owner: string;
+    name: string;
+}
+
+const CommitHistory: React.FC<UserProfilePhotoProps> = ({ username }) => {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [commitsPerDay, setCommitsPerDay] = useState(0);
+    const hasFetchedData = useRef(false);
 
+    const fetchCommitsForRepository = async (owner: string, repo: string) => {
+        // /repos/{owner}/{repo}/commits
+        try {
+            const response = await axios.get<Repository[]>(`https://api.github.com/repos/${owner}/${repo}/commits`,
+            {
+                headers: {
+                    Authorization: `${process.env.GITHUB_READ_API_KEY}`,
+                },
+            })
+            // let prevCommitsPerDay = commitsPerDay;
+            setCommitsPerDay(prevCommitsPerDay => prevCommitsPerDay + response.data.length);
+            // console.log(`Commits in repo ${repo}: ${response.data.length}`);
+        } catch (error: any) {
+            setError(error.message);
+        }
+    }
+    
     useEffect(() => {
         const fetchUserData = async () => {
+            if (hasFetchedData.current)
+                return;
+
             try {
+                hasFetchedData.current = true;
                 setIsLoading(true);
                 setError(null);
-                const response = await axios.get(`https://api.github.com/users/${username}`,
+                const response = await axios.get<Repository[]>(`https://api.github.com/users/${username}/repos`,
                 {
                     headers: {
                         Authorization: `${process.env.GITHUB_READ_API_KEY}`,
                     },
                 })
-                setGithubData(response.data);
+                // setRepositories(response.data.map((repo: any) => { return {owner: repo.owner.login, name: repo.name} }));
+                response.data.forEach((repo: any) => {
+                    // console.log(`Fetching commits in repo ${repo}`);
+                    fetchCommitsForRepository(repo.owner.login, repo.name);
+                })
             } catch (error: any) {
                 setError(error.message);
             } finally {
@@ -35,20 +67,28 @@ const UserProfilePhoto: React.FC<UserProfilePhotoProps> = ({ username }) => {
     }, [])
 
     if (isLoading)
-        return <IconUser className="p-6 rounded-full w-40 h-auto animate-pulse border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700"/>
+        return <p>Loading...</p>
 
     if (error)
         return <p>Error: {error}</p>
 
     return (
-        <img className="rounded-full w-40 h-auto" src={githubData["avatar_url"]}/>
+        <div>
+            <span>Total Commits: {commitsPerDay}</span>
+            {
+                repositories.map((repo: any, index: number) => (
+                    <p key={index}>{repo.owner}: {repo.name}</p>
+                    // <pre>{JSON.stringify(repo.owner, null, 2)}</pre>
+                ))
+            }
+        </div>
     );
 }
 
 const CommitGraph = () => {
     return (
         <div className="gap-2 mt-5 flex flex-col items-center justify-center">
-            <UserProfilePhoto username="ymumberson"/>
+            <CommitHistory username="ymumberson"/>
         </div>
     );
 }
