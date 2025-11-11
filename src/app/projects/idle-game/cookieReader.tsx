5fa343c448327a0e1d1ab99415fbfff308809924
Yoshan Mumbrson <mryosh@outlook.com>
2025-11-11 11:57:59 +0000

    Made simple counter using cookies. First pass so doesn't account for race conditions.

---
+++ src/app/projects/idle-game/cookieReader.tsx
@@ -0,0 +1,13 @@
+"use server"
+import { cookies } from "next/headers";
+
+export async function getCookie(cookieName: string) {
+    const cookie = (await cookies()).get(cookieName);
+    return cookie ? cookie.value : "";
+}
+
+export async function setCookie(cookieName: string, cookieValue: string) {
+    const cookiesData = await cookies();
+    cookiesData.set(cookieName, cookieValue);
+    console.log("Set cookie to value: " + cookieValue);
+}
--- src/app/projects/idle-game/page.tsx
+++ src/app/projects/idle-game/page.tsx
@@ -1,9 +1,40 @@
+'use client';  // This marks the component as a Client Component
+
 import Project from "@/app/components/Project";
+import { getCookie, setCookie } from "./cookieReader";
+import { useEffect, useState } from "react";
+
+const SAVE_DATA_COOKIE = "IdleGameSaveData";
 
 const idleGame = () => {
+    const [cookies, setCookies] = useState(-1);
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        if (cookies === -1) {
+            async function fetchCookies() {
+                const cookiesFetch = await getCookie(SAVE_DATA_COOKIE);
+                setCookies(+cookiesFetch);
+            }
+            fetchCookies();
+        } else {
+            async function setCookies() {
+                await setCookie(SAVE_DATA_COOKIE, cookies.toString());
+            }
+            setCookies();
+        }
+    }, [cookies]);
+
+    async function btnClick() {
+        setCounter(counter+1);
+        setCookies(cookies+1);
+    }
+    
     return (
         <Project name={"Idle Game"} description={"This is a small project to learn about cookies."} >
-            Idle Game
+            <div>Total: {cookies}</div>
+            <div>This session: {counter}</div>
+            <button onClick={btnClick}>Click</button>
         </Project>
     );
 }
