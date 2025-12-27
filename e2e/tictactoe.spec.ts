249f3794c1e77dc3e6d93bd6a6ab0d38380f38ed
Yoshan Mumberson <mryosh@outlook.com>
2025-12-27 23:22:45 +0000

    Added additional tests for the tictac toe game

--- e2e/tictactoe.spec.ts
+++ e2e/tictactoe.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Winning logic works', async ({ page }) => {
+test('X can win', async ({ page }) => {
   await page.goto('');
   await page.getByRole('link', { name: 'Projects' }).click();
   await page.getByRole('link', { name: 'See project' }).first().click();
@@ -10,4 +10,33 @@ test('Winning logic works', async ({ page }) => {
   await page.getByRole('button', { name: 'O' }).nth(2).click();
   await page.getByRole('button', { name: 'X' }).nth(4).click();
   await expect(page.locator('h2')).toContainText('Winner: X');
+});
+
+test('O can win', async ({ page }) => {
+  await page.goto('');
+  await page.getByRole('link', { name: 'Projects' }).click();
+  await page.getByRole('link', { name: 'See project' }).first().click();
+  await page.getByRole('button', { name: 'X' }).first().click();
+  await page.getByRole('button', { name: 'O' }).nth(1).click();
+  await page.getByRole('button', { name: 'X' }).nth(1).click();
+  await page.getByRole('button', { name: 'O' }).nth(2).click();
+  await page.getByRole('button', { name: 'X' }).nth(3).click();
+  await page.getByRole('button', { name: 'O' }).nth(3).click();
+  await expect(page.locator('h2')).toContainText('Winner: O');
+});
+
+test('Draw can happen', async ({ page }) => {
+  await page.goto('');
+  await page.getByRole('link', { name: 'Projects' }).click();
+  await page.getByRole('link', { name: 'See project' }).first().click();
+  await page.getByRole('button', { name: 'X' }).nth(2).click();
+  await page.getByRole('button', { name: 'O' }).nth(3).click();
+  await page.getByRole('button', { name: 'X' }).nth(4).click();
+  await page.getByRole('button', { name: 'O' }).nth(1).click();
+  await page.getByRole('button', { name: 'X' }).nth(5).click();
+  await page.getByRole('button', { name: 'O' }).nth(5).click();
+  await page.getByRole('button', { name: 'X' }).first().click();
+  await page.getByRole('button', { name: 'O' }).nth(1).click();
+  await page.getByRole('button', { name: 'X' }).nth(3).click();
+  await expect(page.locator('h2')).toContainText('Game Over!');
 });
