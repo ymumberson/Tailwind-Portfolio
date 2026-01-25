b9c3eadb125f9e681a746b002bccef3be1a2f368
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-01-25 11:32:47 +0000

    Setup skeleton page for the mongo db integration

---
+++ src/app/projects/mongo-db-example/page.tsx
@@ -0,0 +1,12 @@
+import Project from "@/app/components/Project";
+import React from "react";
+
+const MongoDbExample = () => {
+    return (
+        <Project name="MongoDB Example" description="This uses the MonboDB Atlas integration from Vercel to read and display data from MongoDB">
+            Content
+        </Project>
+    );
+}
+
+export default MongoDbExample;
