8ccf3f891935c16fe0ac4821f1d7ed22b3c3ef06
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-01-25 11:30:53 +0000

    Setup simple skeleton for reference

---
+++ src/app/projects/skeleton/page.tsx
@@ -0,0 +1,12 @@
+import Project from "@/app/components/Project";
+import React from "react";
+
+const Skeleton = () => {
+    return (
+        <Project name="Name" description="Description">
+            Content
+        </Project>
+    );
+}
+
+export default Skeleton;
