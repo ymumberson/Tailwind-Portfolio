b8bf6c115c805c9f8f1a81c20fef164fb22a5207
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-02-15 16:15:36 +0000

    Refactor MongoDB client options to use MongoClientOptions type and change global variable declaration to const

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,14 @@
 /*
 Following on from: https://medium.com/@qedrico/setting-up-atlas-mongodb-in-an-existing-next-js-project-4668ad41682a
 */
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options: MongoClientOptions = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
@@ -16,7 +16,7 @@ let clientPromise: Promise<MongoClient>;
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClient?: MongoClient;
     _mongoClientPromise?: Promise<MongoClient>
   }
