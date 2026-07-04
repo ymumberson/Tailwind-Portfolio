0abe660a271148543f95132bbf8de4049db1cffc
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-07-04 10:51:52 +0100

    Setup simple skeleton for this branch

---
+++ src/app/projects/react-refresher/page.tsx
@@ -0,0 +1,12 @@
+import Project from "@/app/components/Project";
+import React from "react";
+
+const Refresher = () => {
+    return (
+        <Project name="Refresher" description="This is a refresher">
+            Content
+        </Project>
+    );
+}
+
+export default Refresher;
