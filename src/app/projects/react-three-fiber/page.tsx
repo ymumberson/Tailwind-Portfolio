9e5d8f528d518bff5aea2eaa3353cf10ee259513
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-07-05 17:46:23 +0100

    Added in simple keyboard with orbit controls

--- src/app/projects/react-three-fiber/page.tsx
+++ src/app/projects/react-three-fiber/page.tsx
@@ -1,37 +1,30 @@
 "use client"
 import Project from "@/app/components/Project";
 import React, { useRef } from "react";
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { OrbitControls } from "@react-three/drei";
 
-function Box(props) {
-    const meshRef = useRef<THREE.Mesh>(null);
-    useFrame((state, delta) => {
-        if (meshRef.current) {
-            meshRef.current.rotation.x += delta;
-            meshRef.current.rotation.y += delta/2;
-        }
-    });
-
+function Scene() {
+    const gltf = useLoader(GLTFLoader, '/3D_Assets/Keyboard.glb');
     return (
-        <mesh {...props} ref={meshRef}>
-            <boxGeometry args={[1,1,1]} />
-        </mesh>
+        <primitive object={gltf.scene} scale={20} rotation={[Math.PI / 8, 0, 0]} position={[0,0,0]}/>
     );
 }
 
 const ReactThreeFiberDemo = () => {
-    
-
     return (
         <Project name="React Three Fiber Demo" description="Demo of React Three Fiber.">
+            <div className="w-full h-[500px]">
             <Canvas>
                 <ambientLight intensity={Math.PI / 2} />
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
                 <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                <Box position={[-1.2, 0, 0]}/>
-                <Box position={[1.2, 0, 0]}/>
+                <Scene />
+                <OrbitControls />
             </Canvas>
+            </div>
         </Project>
     );
 }
