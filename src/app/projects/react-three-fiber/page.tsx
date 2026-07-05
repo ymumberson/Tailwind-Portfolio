818730f6c1a8749bc6dfa029163c2e7290053d69
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-07-05 18:24:35 +0100

    Updated typos, fixed missed semicolons, added suspense to GLTF loading, and removed unused packes / updated some packages to dev dependencies.

--- src/app/projects/react-three-fiber/page.tsx
+++ src/app/projects/react-three-fiber/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Project from "@/app/components/Project";
 import React from "react";
 import { Canvas } from '@react-three/fiber';
@@ -15,14 +15,16 @@ const ReactThreeFiberDemo = () => {
     return (
         <Project
         name="React Three Fiber Demo"
-        description="A short demo of loading a GLB into React Three Fiber. This demo has orbit controls to manupulate the model. The model is a simple keyboard I modelled in Blender."
+        description="A short demo of loading a GLB into React Three Fiber. This demo has orbit controls to manipulate the model. The model is a simple keyboard I modelled in Blender."
         >
             <div className="w-full h-[500px]">
             <Canvas>
                 <ambientLight intensity={Math.PI / 2} />
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
                 <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                <Scene />
+                <React.Suspense fallback={null}>
+                    <Scene />
+                </React.Suspense>
                 <OrbitControls />
             </Canvas>
             </div>
