434079030544aa90411f8508068efff8972f7a7f
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-07-08 20:22:51 +0100

    Setup initial game logic for inputting guesses

--- src/app/projects/mastermind/page.tsx
+++ src/app/projects/mastermind/page.tsx
@@ -8,20 +8,13 @@ enum TileState {
     INCORECT_VALUE = "Incorrect Value",
 }
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     text: string;
-    onClick?: Function;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
-
-    function handleClick() {
-        if (onClick)
-            onClick();
-    }
-
     return (
-        <button onClick={handleClick} className="px-2 h-10 min-w-10 border-2 text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 font-medium rounded-md text-center dark:border-gray-600 dark:text-gray-400">
+        <button onClick={onClick} className="px-2 h-10 min-w-10 border-2 text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 font-medium rounded-md text-center dark:border-gray-600 dark:text-gray-400">
             {text}
         </button>
     );
@@ -59,9 +52,11 @@ interface TilesProps {
     columnCount: number;
     rowCount: number;
     tiles: string[];
+    targetWord: string;
+    guess: string;
 } 
 
-const Tiles: React.FC<TilesProps> = ({ columnCount, rowCount, tiles }) => {
+const Tiles: React.FC<TilesProps> = ({ columnCount, rowCount, tiles, targetWord, guess }) => {
     return (
         <div className="inline-grid gap-2"
         style={{
@@ -69,7 +64,19 @@ const Tiles: React.FC<TilesProps> = ({ columnCount, rowCount, tiles }) => {
             gridTemplateRows: `repeat(${rowCount}, auto)`,
         }}>
             {tiles.map((tile: string, index: number) => {
-                return <Tile text={tile} tileState={TileState.CORRECT} key={index}/>;
+                let guessIndex = index % columnCount;
+
+                let tileState = TileState.INCORECT_VALUE;
+
+                if (tile === "") {
+                    tileState = TileState.INCORECT_VALUE;
+                } else if (targetWord[guessIndex] === tile) {
+                    tileState = TileState.CORRECT;
+                } else if (targetWord.indexOf(tile) !== -1) {
+                    tileState = TileState.INCORRECT_LOCATION;
+                }
+
+                return <Tile text={tile} tileState={tileState} key={index}/>;
             })}
         </div>
     );
@@ -77,9 +84,10 @@ const Tiles: React.FC<TilesProps> = ({ columnCount, rowCount, tiles }) => {
 
 interface InputRowProps {
     inputLength: number;
+    setGuess: Function;
 }
 
-const InputRow: React.FC<InputRowProps> = ({ inputLength }) => {
+const InputRow: React.FC<InputRowProps> = ({ inputLength, setGuess }) => {
     const [userInput, setUserInput] = useState(Array(inputLength).fill(""));
     const inputRefs = useRef<(HTMLElement | null)[]>([]);
     
@@ -101,8 +109,15 @@ const InputRow: React.FC<InputRowProps> = ({ inputLength }) => {
         }
     }
 
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setGuess(userInput.flat());
+        setUserInput(Array(inputLength).fill(""));
+        inputRefs.current[0]?.focus();
+    }
+
     return (
-        <form className="inline-flex gap-2 pt-5">
+        <form onSubmit={handleSubmit} className="inline-flex gap-2 pt-5">
             {Array.from({length: inputLength}).map((_, index: number) => {
                 return <input
                             key={index}
@@ -114,9 +129,10 @@ const InputRow: React.FC<InputRowProps> = ({ inputLength }) => {
                             onKeyDown={(e) => handleKeyDown(index, e)}
                             onFocus={(e) => e.target.select()}
                             maxLength={1}
+                            required
                             className="px-2 h-10 w-10 border-2 text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 font-medium rounded-md text-center dark:border-gray-600 dark:text-gray-400"/>
             })}
-            <Button text="Submit"/>
+            <Button text="Submit" type="submit"/>
         </form>
     )
 }
@@ -125,15 +141,31 @@ const Mastermind = () => {
     const numberOfGuesses = 8;
     const numberOfTiles = 5;
     const [tiles, setTiles] = useState(Array<string>(numberOfGuesses*numberOfTiles).fill("")); 
+    const [targetWord, setTargetWord] = useState("apple");
+    const [guess, setGuess] = useState("");
+    const [guessCount, setGuessCount] = useState(0);
+
+    function handleGuess(guess: string) {
+        if (guessCount >= numberOfGuesses) 
+            return;
+
+        let newTiles = tiles.slice();
+        for (let i=0; i<guess.length; ++i) {
+            newTiles[guessCount * numberOfTiles + i] = guess[i];
+        }
+
+        setTiles(newTiles);
+        setGuessCount(guessCount + 1);
+    }
 
     return (
         <Project name="Mastermind" description="Description">
             <div className="">
-                <Tiles tiles={tiles} columnCount={numberOfTiles} rowCount={numberOfGuesses}/>
+                <Tiles tiles={tiles} columnCount={numberOfTiles} rowCount={numberOfGuesses} targetWord={targetWord} guess={guess}/>
                 <div>
 
                 </div>
-                <InputRow inputLength={numberOfTiles}/>
+                <InputRow inputLength={numberOfTiles} setGuess={handleGuess}/>
             </div>
         </Project>
     );
