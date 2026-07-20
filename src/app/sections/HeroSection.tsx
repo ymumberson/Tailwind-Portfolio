e3cbfb8c15ea9ab26b9ea36dbf365cd29f449e0c
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-07-20 23:43:06 +0100

    Added in initial pass of 3D desk into hero section. Currently setup to take half of the screen.

--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -1,5 +1,16 @@
+'use client';
 import React from "react";
 import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function Scene() {
+    const { scene }  = useGLTF('/3D_Assets/PopulatedDesk.glb');
+    return (
+        <primitive object={scene} scale={1} rotation={[0, 0, 0]} position={[0,0,0]}/>
+    );
+}
+
 
 const HeroSection = () => {
     return (
@@ -9,6 +20,17 @@ const HeroSection = () => {
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
                 I'm a software engineer with experience in simulation, video game development, ray tracing, data visualisation, and machine learning.
             </p>
+        </div>
+        <div className="h-[50vh] w-full">
+            <Canvas camera={{ fov: 60, position: [0, 1, 1.25]}}>
+                <ambientLight intensity={Math.PI / 2} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                <React.Suspense fallback={null}>
+                    <Scene />
+                </React.Suspense>
+                <OrbitControls target={[0,0.5,0]} enableDamping/>
+            </Canvas>
         </div>
         <div className="flex justify-center gap-2">
             <a href="https://www.linkedin.com/in/yoshanmumberson/" className="text-white bg-[#004182] hover:bg-[#004182]/90 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#004182]/90 mb-2 gap-1">
