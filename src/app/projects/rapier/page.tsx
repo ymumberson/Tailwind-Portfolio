6b28b7154a9a3210fc77f0646111aa437558b9d5
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-07-26 10:34:58 +0100

    Added description to the page.

--- src/app/projects/rapier/page.tsx
+++ src/app/projects/rapier/page.tsx
@@ -30,7 +30,10 @@ const Rapier = () => {
     const [itemSpawnRate, setItemSpawnRate] = useState(1);
 
     return (
-        <Project name="Rapier" description="Testing out the Rapier physics engine">
+        <Project
+            name="Rapier"
+            description="Testing out the Rapier physics engine alongside React Three Fiber. This demo uses the cup model with 2 simple cylinder colliders to try and improve performance. This means that each cup is a RigidBody with 2 colliders. You can click on any of the cups to make them jump into the air. Enable debug mode to see the colliders and frame rate, or change the spawn settings. Max items controls how many items can be on the screen, and spawn rate controls how many seconds there are between item spawns. Otherwise this uses the standard OrbitControls so you can pan and zoom etc."
+        >
             <Controls debugMode={debugMode} setDebugMode={setDebugMode} maxItems={maxItems} setMaxItems={setMaxItems} itemSpawnRate={itemSpawnRate} setItemSpawnRate={setItemSpawnRate}/>
             <div className="h-[500px]">
                 <RapierCanvas debugMode={debugMode} maxItems={maxItems} itemSpawnRate={itemSpawnRate}/>
