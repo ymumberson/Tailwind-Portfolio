0bd84bb4c78ad8fcbf5422c9b97b912ce3d2aed1
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-08-09 22:08:21 +0100

    Updated projects to be in chronological order

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,13 +6,13 @@ export default function Projects() {
       <div className="sm:px-16 xl:px-48">
         <h1 className="text-center mb-4 text-3xl font-extrabold tracking-light leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Projects</h1>
         <div className="flex flex-col items-center gap-2">
-            <Card title="Tic-Tac-Toe" href="/projects/tic-tac-toe" hrefText="See project" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe" />
-            <Card title="Digit Classification" href="/projects/digit-classification" hrefText="See project" description="Attempting to classify hand-drawn digits by using a Neural Network trained on the MNIST hand-drawn digits dataset. Digits can be from 0 to 9, however, the model will always predict one of these values. This is the reason that even the empty canvas predicts to a digit." />
-            <Card title="Weather" href="/projects/weather" hrefText="See project" description="Fetching current weather data from https://openweathermap.org/ and displaying it." />
-            <Card title="MongoDB Example" href="/projects/mongo-db-example" hrefText="See project" description="A simple example of using MongoDB Atlas." />
-            <Card title="React Three Fiber Demo" href="/projects/react-three-fiber" hrefText="See project" description="A short demo of React Three Fiber." />
-            <Card title="Wordle" href="/projects/wordle" hrefText="See project" description="Making a simple clone of Wordle." />
             <Card title="Rapier" href="/projects/rapier" hrefText="See project" description="Testing out the Rapier physics engine." />
+            <Card title="Wordle" href="/projects/wordle" hrefText="See project" description="Making a simple clone of Wordle." />
+            <Card title="React Three Fiber Demo" href="/projects/react-three-fiber" hrefText="See project" description="A short demo of React Three Fiber." />
+            <Card title="MongoDB Example" href="/projects/mongo-db-example" hrefText="See project" description="A simple example of using MongoDB Atlas." />
+            <Card title="Weather" href="/projects/weather" hrefText="See project" description="Fetching current weather data from https://openweathermap.org/ and displaying it." />
+            <Card title="Digit Classification" href="/projects/digit-classification" hrefText="See project" description="Attempting to classify hand-drawn digits by using a Neural Network trained on the MNIST hand-drawn digits dataset. Digits can be from 0 to 9, however, the model will always predict one of these values. This is the reason that even the empty canvas predicts to a digit." />
+            <Card title="Tic-Tac-Toe" href="/projects/tic-tac-toe" hrefText="See project" description="Following the Tic-Tac-Toe tutorial from https://react.dev/learn/tutorial-tic-tac-toe" />
         </div>
       </div>
     );
