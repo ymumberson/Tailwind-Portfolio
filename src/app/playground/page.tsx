f39891b9072a8ba10182511053dfcf0b1fdc4654
Yoshan Mumberson <73796199+ymumberson@users.noreply.github.com>
2026-08-30 13:59:07 +0100

    Renamed projects to playground, and added in palceholder route and page for the projects which is where the github projects will live.

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,22 +1,14 @@
-import React from "react";
 import Card from "../components/Card";
-import { IconBrandGithub } from "@tabler/icons-react";
 
-export default function Projects() {
+export default function Playground() {
     return (
       <div className="sm:px-16 xl:px-48">
         <h1 className="text-center mb-4 text-3xl font-extrabold tracking-light leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
-          Projects
+          Playground
         </h1>
-        <p className="mx-auto text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 max-w-lg">
-          These are larger more complete projects, but are mostly non-interactive. This list is a subset fetched from my GitHub, but you can click the button below to see all of my projects.
+        <p className="mx-auto mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 max-w-lg">
+          These are small coding experiements to learn something new, with a focus on being interactive.
         </p>
-        <div className="w-full flex justify-center my-4">
-            <a href="https://github.com/ymumberson" className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#24292F]/60 mb-2 gap-1">
-                <IconBrandGithub size={20}/>
-                Github
-            </a>
-        </div>
         <div className="flex flex-col items-center gap-2">
             <Card title="Rapier" href="/playground/rapier" hrefText="See project" description="Testing out the Rapier physics engine." />
             <Card title="Wordle" href="/playground/wordle" hrefText="See project" description="Making a simple clone of Wordle." />
